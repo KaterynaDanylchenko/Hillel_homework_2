353b9376368b31eb949505eadc0fc4fb625c3ea0
KaterynaDanylchenko <108829987+KaterynaDanylchenko@users.noreply.github.com>
2022-07-31 10:40:46 +0300

    Update Homework_8_game.py

--- 2/Homework_8_game.py
+++ 2/Homework_8_game.py
@@ -1,8 +1,8 @@
-from Homework_8_functions import figure_choice,figure_game
 # Напишіть гру "Камінь ножиці папір". Схема роботи така:
 # користувачу виводиться повідомлення зі списком фігур.
 # користувач обриає якусь фігуру, програма випадковим чином обирає свою.
 # визначається переможець
+from Homework_8_functions import figure_choice,figure_game
 figure_choice()
 user_massage=input('>>>>>')
 figure_game(user_massage)
