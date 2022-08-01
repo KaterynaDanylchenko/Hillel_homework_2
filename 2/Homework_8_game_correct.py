0e33af5e5cbc0e4995d453146a482aa5627b9114
KaterynaDanylchenko <108829987+KaterynaDanylchenko@users.noreply.github.com>
2022-08-01 18:52:09 +0300

    Add files via upload

---
+++ 2/Homework_8_game_correct.py
@@ -0,0 +1,5 @@
+from Homework_8_functions_correct import computer_choice,figure_game,figure_choice_by_user
+figure_choice_by_user=figure_choice_by_user()
+computer_choice=computer_choice()
+figure_game(figure_choice_by_user,computer_choice)
+
